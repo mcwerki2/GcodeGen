5c3f971aeb3ac6039d0fb6292179d92de8713305
mcwerki2 <mcwerki2@gmail.com>
2024-01-14 15:13:39 +0100

    Fixed Typos and added docstrings

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import json
 import os.path
 
-info_tabelle = {"+X_+Y": "X_Y-_X-_Y",
+info_tabelle = {"+X_+Y": "X_Y-_X-_Y",  # Eine Tabelle die der code benötigt, um Richtungen herauszufinden
                 "+X_-Y": "X_Y_X-_Y-",
                 "-X_+Y": "X-_Y-_X_Y",
                 "-X_-Y": "X-_Y_X_Y-",
@@ -15,8 +15,9 @@ info_tabelle = {"+X_+Y": "X_Y-_X-_Y",
 
 
 def get_user_input():
-    """Bekommt die Inputs des Benutzers"""
-    if input("Test (y/n)?: ").lower() == "y":
+    """Erfragt die eingaben des Benutzers"""
+    input_temp = input("Test (j/n)?: ").lower()
+    if input_temp == "j" or input_temp == "y" or input_temp == "":
         with open("test.json", "r") as f:
             output_user = json.load(f)
 
@@ -24,7 +25,7 @@ def get_user_input():
         vorschub_ges = int(input("Vorschub (mm/min): "))
         zustellung = float(input("Zustellung (mm): "))
         schnitt_tiefe = int(input("Tiefe des Schnitts (mm): "))
-        # werkzeug_radius = int(input("Fräser Radius (mm): "))
+        spindel_ges = int(input("Spindel geschwindigkeit (rpm): "))
         len_des_schnitts = int(input("Länge des Schnitts (mm): "))
         x_position_schnitt_anfang = int(input("X Position Schnitt Anfang (mm): "))
         y_position_schnitt_anfang = int(input("Y Position Schnitt Anfang (mm): "))
@@ -33,13 +34,16 @@ def get_user_input():
         lage_des_materials = input("Lage des Materials (+X, -Y, +Y, -Y): ").upper()
         datei_name = input("Dateiname(str): ").replace(" ", "")
         sicherheitsabstand = int(input("sicherheitsabstand (größer als fräser radius) (mm): "))
-        output_user: dict = {"vorschub_ges": vorschub_ges, "zustellung": zustellung, "schnitt_tiefe": schnitt_tiefe, "len_des_schnitts": len_des_schnitts, "x_position_schnitt_anfang": x_position_schnitt_anfang,
-                             "y_position_schnitt_anfang": y_position_schnitt_anfang, "z_position_schnitt_anfang": z_position_schnitt_anfang, "richtung_des_schnitts": richtung_des_schnitts, "lage_des_materials": lage_des_materials, "datei_name": datei_name, "sicherheitsabstand": sicherheitsabstand} # NOQA
+        output_user: dict = {"vorschub_ges": vorschub_ges, "zustellung": zustellung, "schnitt_tiefe": schnitt_tiefe, "spindel_ges": spindel_ges, "len_des_schnitts": len_des_schnitts,
+                             "x_position_schnitt_anfang": x_position_schnitt_anfang, "y_position_schnitt_anfang": y_position_schnitt_anfang, "z_position_schnitt_anfang": z_position_schnitt_anfang,
+                             "richtung_des_schnitts": richtung_des_schnitts, "lage_des_materials": lage_des_materials, "datei_name": datei_name, "sicherheitsabstand": sicherheitsabstand} # NOQA
         print("\n\n")
     return output_user
 
 
 def write_file(name, cont):
+    """Schreibt den fertigen Gcode in eine datei."""
+
     if os.path.isfile(f"./{name}.nc"):
         print("Datei existiert! nicht geschrieben")
         if input("Trotzdem schreiben (y/n)?: ").lower() == "n":
@@ -50,6 +54,7 @@ def write_file(name, cont):
             with open(f"{name}.nc", "a") as f:
                 for i in cont:
                     f.write(i)
+                    f.write("\n")
             print("erfolgreich geschrieben")
     else:
         with open(f"{name}.nc", "a") as f:
@@ -60,12 +65,17 @@ def write_file(name, cont):
 
 
 def cut_para(input_user, info_liste):
+    """Benutzt die eingaben des Benutzers mithilfe einer liste, um herauszufinden,
+    in welcher seite weggestellt werden kann"""
+
     list_search = f"{input_user['richtung_des_schnitts']}_{input_user['lage_des_materials']}"
     result = info_liste[list_search].split("_")
     return result
 
 
 def cycle_define(input_user, info_liste):
+    """Schreibt den Gcode in eine liste und gib dies zurück"""
+
     output: list[str] = []
     len_des_schnitts = input_user["len_des_schnitts"] + input_user["sicherheitsabstand"] + input_user["sicherheitsabstand"] + input_user["zustellung"]
     
@@ -75,6 +85,7 @@ def cycle_define(input_user, info_liste):
     output.append(f"G0 X{input_user['x_position_schnitt_anfang'] - input_user['sicherheitsabstand'] - input_user['zustellung']} Y{input_user['y_position_schnitt_anfang'] - input_user['sicherheitsabstand'] - input_user['zustellung']} Z{input_user['z_position_schnitt_anfang']}") # NOQA
 
     output.append(f"\nG91 (Relative Positionierung)\n")
+    output.append(f"M03 S{input_user['spindel_ges']}")
 
     # Eigentliche Logik
 
@@ -91,6 +102,7 @@ def cycle_define(input_user, info_liste):
     output.append(f"G1 {para_list[0]}{len_des_schnitts} F{input_user['vorschub_ges']}")
     output.append(f"G0 {para_list[1]}{input_user['sicherheitsabstand']}")
     output.append(f"G0 {para_list[2]}{len_des_schnitts}\n")
+    output.append(f"\nM05")
     output.append(f"\nM30\n")
 
     print("\n")
@@ -100,6 +112,7 @@ def cycle_define(input_user, info_liste):
     return output
 
 
-temp1 = get_user_input()
-temp2 = cycle_define(temp1, info_tabelle)
-write_file(temp1["datei_name"], temp2)
+if __name__ == '__main__':
+    temp1 = get_user_input()
+    temp2 = cycle_define(temp1, info_tabelle)
+    write_file(temp1["datei_name"], temp2)
